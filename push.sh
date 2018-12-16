c2446eaf3c3a2090e2a3aff6544fe0297ff4dd88
Witee <xieweiting@admaster.com.cn>
2018-12-16 12:42:30 +0800

    update docs

--- push.sh
+++ push.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+read -p "是否需要更新 npm run docs: (y/N)" run_docs
+if [ "$run_docs" == 'y' -o "$run_docs" == 'Y' ]
+then
+  echo 'npm run docs...'
+  npm run docs
+  echo 'npm run docs... done'
+else
+  echo 'npm run docs... canceled...'
+fi
+
 git status
 
 read -p "请输入 commit 信息(n 取消; 回车默认): " commitMsg
