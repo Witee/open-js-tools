7adfc6ea70d438bbbe1440914fd74aea0cd831aa
Witee <525942866@qq.com>
2021-09-22 12:16:37 +0800

    update docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,13 +1,13 @@
 define({
   "name": "open-js-tools",
-  "version": "2.0.1",
+  "version": "2.1.0",
   "description": "JS 工具库",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-09-22T04:09:45.040Z",
+    "time": "2021-09-22T04:16:31.066Z",
     "url": "http://apidocjs.com",
     "version": "0.22.1"
   }
